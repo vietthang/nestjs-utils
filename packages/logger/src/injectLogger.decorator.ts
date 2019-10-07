ed1cdb064ab787a82ec4b29fa4357f900509ec82
Thang Nguyen <thangnv77@gmail.com>
2019-10-07 23:56:37 +0700

    Fix InjectLogger decorator

--- packages/logger/src/injectLogger.decorator.ts
+++ packages/logger/src/injectLogger.decorator.ts
@@ -10,27 +10,25 @@ export const registeredProviders: FactoryProvider[] = []
 export function InjectLogger(
   nameOrClass?: string | (new (...args: any[]) => any),
   options?: LoggerOptions,
-): MethodDecorator {
-  return () => {
-    const key = [prefix, name].join('__')
-    registeredProviders.push({
-      provide: key,
-      useFactory(defaultOptions?: LoggerOptions): Logger {
-        return createLogger({
-          ...defaultOptions,
-          ...options,
-          defaultMeta: {
-            ...(defaultOptions && defaultOptions.defaultMeta),
-            ...(options && options.defaultMeta),
-            tag:
-              name && typeof nameOrClass === 'function'
-                ? nameOrClass.name
-                : nameOrClass,
-          },
-        })
-      },
-      inject: [LOGGER_CONFIG_SYMBOL],
-    })
-    Inject(key)
-  }
+) {
+  const key = [prefix, name].join('__')
+  registeredProviders.push({
+    provide: key,
+    useFactory(defaultOptions?: LoggerOptions): Logger {
+      return createLogger({
+        ...defaultOptions,
+        ...options,
+        defaultMeta: {
+          ...(defaultOptions && defaultOptions.defaultMeta),
+          ...(options && options.defaultMeta),
+          tag:
+            name && typeof nameOrClass === 'function'
+              ? nameOrClass.name
+              : nameOrClass,
+        },
+      })
+    },
+    inject: [LOGGER_CONFIG_SYMBOL],
+  })
+  return Inject(key)
 }
