611df665c66ed9d914b3e0f00e4e5a7b8cfb7b81
Thang Nguyen <thangnv77@gmail.com>
2019-10-16 20:39:56 +0700

    Revert error util fix function

--- packages/common/src/error-util.ts
+++ packages/common/src/error-util.ts
@@ -155,7 +155,7 @@ export async function wrapAsync<T>(
   try {
     return await promise
   } catch (origin) {
-    const error = base.extend({ origin: errorTransform(origin) })
+    const error = base.extend(errorTransform(origin))
     error.stack = base.stack
     throw error
   }
