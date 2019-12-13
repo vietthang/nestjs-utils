d2c16b4cb431bf0b3b025c67f0417ed7e9ca76bb
Thang Nguyen <thangnv77@gmail.com>
2019-12-13 13:35:14 +0700

    Update dependencies

--- packages/validation/src/utils.ts
+++ packages/validation/src/utils.ts
@@ -1,23 +1,9 @@
 import { Type } from '@nestjs/common'
 
 let currentId = 0
-const objectIdMap = new WeakMap<object, string>()
 
 export function nextId(): string {
   return (currentId++).toString()
-}
-
-export function objectSerializer(o: object): string {
-  const cached = objectIdMap.get(o)
-
-  if (cached !== undefined) {
-    return cached
-  }
-  const id = nextId()
-
-  objectIdMap.set(o, id)
-
-  return id
 }
 
 export function namedClass<C extends Type<{}>>(base: C, name: string): C {
